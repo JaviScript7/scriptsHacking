e508b019beb00979d6ec67b62f1d36fec785a4da
JaviScript7 <javierlosa00@gmail.com>
2024-10-06 16:54:34 -0600

    Extraemos información del OS

--- extraerinfo.py
+++ extraerinfo.py
@@ -1,10 +1,38 @@
 from subprocess import check_output
 import subprocess 
+import platform
 
-sistema = check_output('systeminfo',stderr = subprocess.STDOUT)
 
-registro = open('resgistro.txt','w+')
-registro.write(sistema)
-print("Confirmacion de informacion sustraida")
-registro.close()
+def detectar_os():
+    #Detectar el sistema operativo
+    sistema_os = platform.system()
+
+    if sistema_os == 'Windows':
+        print("Es sistema Windows")
+         ##Este script funciona solo en windows ya que SYSTEMINFO es solo de windows 
+        sistema = check_output('systeminfo',stderr = subprocess.STDOUT).decode('UTF-8')
+        return sistema
+
+    elif sistema_os in ['Linux','Darwin']: #Darwin es para MacOs
+        print("Es sistema Unix")
+         ##Para sistemas con base unix usaremos el siguiente codigo
+        sistema = check_output(['uname', '-a'], stderr=subprocess.STDOUT).decode('UTF-8')
+        return sistema
+    else:
+        return "Sistema no reconocido"
+        
+try:
+    
+    sistem_info = detectar_os()
+
+    if sistem_info:
+        registro = open('registro.txt','w+')
+        registro.write(sistem_info)
+        print("Confirmacion de informacion sustraida")
+        registro.close()
+
+
+except Exception as e:
+    print("Ocurrio un error, no se reconoce el sistema operativo",e)
+
 
